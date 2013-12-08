419a4459eccc27ebdbdb570b5058a16a6cc106ae
yeokm1 <yeokm1@yahoo.com.sg>
2013-12-08 21:17:11 +0800

    Add link to preference list to reset password

--- NUSSOCPrint/src/ui/SettingsFragment.java
+++ NUSSOCPrint/src/ui/SettingsFragment.java
@@ -4,9 +4,13 @@ package ui;
 
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 
 import com.yeokm1.nussocprintandroid.R;
 
@@ -33,7 +37,19 @@ public class SettingsFragment extends PreferenceListFragment implements OnShared
         addPreferencesFromResource(R.layout.preference_layout);
         
         sharedPref = getPreferenceScreen().getSharedPreferences();
-        sharedPref.registerOnSharedPreferenceChangeListener(this);    
+        sharedPref.registerOnSharedPreferenceChangeListener(this);  
+        
+        Preference resetLink = findPreference(getString(R.string.preference_reset_unix_password_link_key));
+        resetLink.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				String url = getString(R.string.reset_unix_password_link);
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+				startActivity(browserIntent);
+				return true;
+			}
+		});
 
     }
 
