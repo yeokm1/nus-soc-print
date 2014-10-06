642f07145cdd79f2e99012bd7fef8430b0a2465e
Yeo Kheng Meng <yeokm1@gmail.com>
2014-10-06 15:44:08 +0800

    status dialog activity

--- NUSSOCPrint/app/src/main/java/com/yeokm1/nussocprint/fragments/PrintFragment.java
+++ NUSSOCPrint/app/src/main/java/com/yeokm1/nussocprint/fragments/PrintFragment.java
@@ -1,14 +1,16 @@
 package com.yeokm1.nussocprint.fragments;
 
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.yeokm1.nussocprint.R;
+import com.yeokm1.nussocprint.print_activities.StatusActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,9 +22,28 @@ public class PrintFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_print, container, false);
+        View view = inflater.inflate(R.layout.fragment_print, container, false);
+
+        Button statusButton = (Button) view.findViewById(R.id.print_status_button);
+        statusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startStatusActivity();
+            }
+        });
+
+
+        return view;
     }
+
+
+    public void startStatusActivity(){
+        Intent intent = new Intent(getActivity(), StatusActivity.class);
+        startActivity(intent);
+    }
+
+
+
 
 
 }
---
+++ NUSSOCPrint/app/src/main/java/com/yeokm1/nussocprint/print_activities/StatusActivity.java
@@ -0,0 +1,18 @@
+package com.yeokm1.nussocprint.print_activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.yeokm1.nussocprint.R;
+
+public class StatusActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+
+
+
+}
