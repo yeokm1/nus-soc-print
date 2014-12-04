345b06098a8b1a7b8a6cbea408ee946130f28df3
Yeo Kheng Meng <yeokm1@gmail.com>
2014-12-04 21:05:59 +0800

    check incoming path is valid

--- app/src/main/java/com/yeokm1/nussocprintandroid/core/MyApplication.java
+++ app/src/main/java/com/yeokm1/nussocprintandroid/core/MyApplication.java
@@ -3,6 +3,9 @@ package com.yeokm1.nussocprintandroid.core;
 import android.app.Application;
 import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.yeokm1.nussocprintandroid.R;
 
 /**
  * Created by yeokm1 on 6/10/2014.
@@ -19,7 +22,41 @@ public class MyApplication extends Application{
     public void setCurrentDocumentUri(Uri newDocumentUri) {
         String path = newDocumentUri.getPath();
         Log.i(TAG, "incoming path " + path);
-        this.currentDocumentPath = newDocumentUri.getPath();
+
+        if(isFileFormatSupported(path)) {
+            this.currentDocumentPath = path;
+        } else {
+            Toast.makeText(this, R.string.print_file_not_supported, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private boolean isFileFormatSupported(String path){
+        if(path.contains(".")) {
+            String extension = path.substring(path.lastIndexOf("."));
+
+            if(extension == null || extension.length() == 0){
+                return false;
+            }
+
+            if(extension.equalsIgnoreCase(".pdf")
+                    || (extension.equalsIgnoreCase(".doc"))
+                    || (extension.equalsIgnoreCase(".docx"))
+                    || (extension.equalsIgnoreCase(".ppt"))
+                    || (extension.equalsIgnoreCase(".pptx"))
+                    || (extension.equalsIgnoreCase(".odt"))){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+
+
+
     }
 
     public void onCreate(){
