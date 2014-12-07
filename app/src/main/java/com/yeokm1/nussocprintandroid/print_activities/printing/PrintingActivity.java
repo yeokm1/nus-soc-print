ec82b1ac7ac14b0815d6ffcc02e11fe4447e53ab
Yeo Kheng Meng <yeokm1@gmail.com>
2014-12-07 22:50:30 +0800

    some major non tested changes

--- app/src/main/java/com/yeokm1/nussocprintandroid/print_activities/printing/PrintingActivity.java
+++ app/src/main/java/com/yeokm1/nussocprintandroid/print_activities/printing/PrintingActivity.java
@@ -2,6 +2,7 @@ package com.yeokm1.nussocprintandroid.print_activities.printing;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
@@ -22,29 +23,33 @@ public class PrintingActivity extends FatDialogActivity {
 
     private static final int INVALID_INTENT_INT = 0;
 
+    private final int POSITION_CONNECTING = 0;
+    private final int POSITION_HOUSEKEEPING = 1;
+    private final int POSITION_DOWNLOADING_DOC_CONVERTER = 2;
+    private final int POSITION_UPLOADING_PDF_CONVERTER = 3;
+    private final int POSITION_UPLOADING_USER_DOC = 4;
+    private final int POSITION_CONVERTING_TO_PDF = 5;
+    private final int POSITION_TRIM_PDF_TO_PAGE_RANGE = 6;
+    private final int POSITION_FORMATTING_PDF = 7;
+    private final int POSITION_CONVERTING_TO_POSTSCRIPT = 8;
+    private final int POSITION_SENDING_TO_PRINTER = 9;
+    private final int POSITION_COMPLETED = 10;
+
     private ListView printProgress;
     private Button finishButton;
 
-
     private String filePath;
-    private String printerName;
+    private String filename;
+    private String printer;
     private int pagesPerSheet;
     private int startPageRange;
     private int endPageRange;
 
-    private final int POSITION_CONNECTING = 0;
-    private final int POSITION_HOUSEKEEPING = 1;
-    private final int POSITION_DOWNLOADING_DOC_CONVERTER = 2;
-    private int POSITION_UPLOADING_PDF_CONVERTER = 3;
-    private int POSITION_UPLOADING_USER_DOC = 4;
-    private int POSITION_CONVERTING_TO_PDF = 5;
-    private int POSITION_TRIM_PDF_TO_PAGE_RANGE = 6;
-    private int POSITION_FORMATTING_PDF = 7;
-    private int POSITION_CONVERTING_TO_POSTSCRIPT = 8;
-    private int POSITION_SENDING_TO_PRINTER = 9;
-    private int POSITION_COMPLETED = 10;
 
-    private String[] PROGRESS_TEXT;
+    private String[] HEADER_TEXT;
+    private String SUBTITLE_PROGRESS_TEXT;
+    private String SUBTITLE_INDETERMINATE_TEXT;
+
 
     private boolean[] PROGRESS_INDETERMINATE =
             {true
@@ -59,10 +64,18 @@ public class PrintingActivity extends FatDialogActivity {
                     ,true};
 
 
-    boolean needToConvertDocToPDF = true;
-    boolean needToFormatPDF = true;
-    boolean needToTrimPDFToPageRange = false;
+    private boolean needToConvertDocToPDF = true;
+    private boolean needToFormatPDF = true;
+    private boolean needToTrimPDFToPageRange = false;
 
+    private int pdfConvSize = 0;
+    private int pdfConvUploaded = 0;
+
+    private int docToPrintSize = 0;
+    private int docToPrintUploaded = 0;
+
+    private int currentProgress = 0;
+    private PrintingTask printingTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,21 +86,26 @@ public class PrintingActivity extends FatDialogActivity {
         printProgress = (ListView) findViewById(R.id.printing_list);
         finishButton = (Button) findViewById(R.id.printing_button_finish);
 
-        PROGRESS_TEXT = getResources().getStringArray(R.array.status_title_text);
+        HEADER_TEXT = getResources().getStringArray(R.array.printing_progress_title_text);
+        SUBTITLE_PROGRESS_TEXT = getString(R.string.printing_progress_subtitle_progress);
+        SUBTITLE_INDETERMINATE_TEXT = getString(R.string.printing_progress_subtitle_progress_indeterminate);
 
         Intent intent = getIntent();
 
         filePath = intent.getStringExtra(INTENT_FILE_PATH);
-        printerName = intent.getStringExtra(INTENT_PRINTER_NAME);
+
+        Uri fileUri =  Uri.parse(filePath);
+        filename = fileUri.getLastPathSegment();
+
+
+        printer = intent.getStringExtra(INTENT_PRINTER_NAME);
         pagesPerSheet = intent.getIntExtra(INTENT_PAGES_PER_SHEET, INVALID_INTENT_INT);
         startPageRange = intent.getIntExtra(INTENT_PAGE_START_RANGE, INVALID_INTENT_INT);
         endPageRange = intent.getIntExtra(INTENT_PAGE_END_RANGE, INVALID_INTENT_INT);
 
-
         if(pagesPerSheet == 1){
             needToFormatPDF = false;
         }
-
 
         if(isFileAPdf(filePath)){
             needToConvertDocToPDF = false;
@@ -97,21 +115,7 @@ public class PrintingActivity extends FatDialogActivity {
             needToTrimPDFToPageRange = true;
         }
 
-        ArrayList<PrintingProgressItem> items = new ArrayList<PrintingProgressItem>();
-        for (int i = 0; i < 5; i++) {
-            String title = "Uploading DOC converter";
-            String description = "This could take a while...";
-            PrintingProgressItem item = new PrintingProgressItem(title, description, true, false);
-            items.add(item);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            String title = "Uploading PDF converter";
-            String description = "This could take a while...";
-            PrintingProgressItem item = new PrintingProgressItem(title, description, 0.5f, false, true);
-            items.add(item);
-        }
-
+        refreshList();
     }
 
 
@@ -119,6 +123,80 @@ public class PrintingActivity extends FatDialogActivity {
     public void refreshList(){
         ArrayList<PrintingProgressItem> items = new ArrayList<PrintingProgressItem>();
 
+        for(int row = 0; row < HEADER_TEXT.length; row++){
+
+            if(row == POSITION_DOWNLOADING_DOC_CONVERTER && !needToConvertDocToPDF
+                    || row == POSITION_CONVERTING_TO_PDF && !needToConvertDocToPDF
+                    || row == POSITION_UPLOADING_PDF_CONVERTER && !needToFormatPDF
+                    || row == POSITION_FORMATTING_PDF && !needToFormatPDF
+                    || row == POSITION_TRIM_PDF_TO_PAGE_RANGE && !needToTrimPDFToPageRange){
+                //Don't have to do
+                continue;
+            }
+
+            String title;
+            String subtitle;
+
+
+            if(row == POSITION_UPLOADING_USER_DOC){
+                title = String.format(HEADER_TEXT[row], filename);
+            } else if(row == POSITION_FORMATTING_PDF){
+                title = String.format(HEADER_TEXT[row], pagesPerSheet);
+            } else if(row == POSITION_SENDING_TO_PRINTER){
+                title = String.format(HEADER_TEXT[row], printer);
+            } else if(row == POSITION_TRIM_PDF_TO_PAGE_RANGE){
+                title = String.format(HEADER_TEXT[row], startPageRange, endPageRange);
+            } else {
+                title = HEADER_TEXT[row];
+            }
+
+            boolean isThisDone;
+            boolean isError;
+
+
+            if(currentProgress > row){
+                isThisDone = true;
+                isError = false;
+            } else if(row == currentProgress){
+                isThisDone = false;
+                if(printingTask == null){
+                    //Means the operation has ended on the current progress, show a cross to mean an error
+                    isError = true;
+                } else {
+                    isError = false;
+                }
+            } else {
+                isThisDone = false;
+                isError = false;
+            }
+
+            float progressFraction = 0;
+            boolean progressIndeterminate = PROGRESS_INDETERMINATE[row];
+
+            if(row == POSITION_UPLOADING_PDF_CONVERTER){
+
+                subtitle = generateProgressString(pdfConvUploaded, pdfConvSize);
+                progressFraction = generateProgressFraction(pdfConvUploaded, pdfConvSize);
+
+            } else if(row == POSITION_UPLOADING_USER_DOC){
+
+                subtitle = generateProgressString(docToPrintUploaded, docToPrintSize);
+                progressFraction = generateProgressFraction(docToPrintUploaded, docToPrintSize);
+            } else {
+               subtitle = SUBTITLE_INDETERMINATE_TEXT;
+            }
+
+            PrintingProgressItem item;
+            if(progressIndeterminate){
+                item = new PrintingProgressItem(title, subtitle, isThisDone, isError);
+            } else {
+                item = new PrintingProgressItem(title, subtitle, progressFraction, isThisDone, isError);
+            }
+
+
+
+            items.add(item);
+        }
 
 
 
@@ -126,6 +204,24 @@ public class PrintingActivity extends FatDialogActivity {
 
         printProgress.setAdapter(new PrintingProgressItemAdapter(this, items));
     }
+
+    private String generateProgressString(long currentSize, long totalSize){
+        String currentSizeStr = humanReadableByteCount(currentSize, true);
+        String totalSizeStr = humanReadableByteCount(totalSize, true);
+
+        String progressStr = String.format(SUBTITLE_PROGRESS_TEXT, currentSizeStr, totalSizeStr);
+        return progressStr;
+    }
+
+    private float generateProgressFraction(long currentSize, long totalSize){
+        double currentSizeDbl = currentSize;
+        double totalSizeDbl = totalSize;
+
+        float fraction = currentSize / totalSize;
+        return fraction;
+    }
+
+
 
 
     private boolean isFileAPdf(String path){
@@ -136,6 +232,15 @@ public class PrintingActivity extends FatDialogActivity {
         } else {
             return false;
         }
+    }
+
+    //http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 
     class PrintingTask extends ConnectionTask {
