ad60a4990ea7aec2480a0b985aee6ac8f6c27f9b
Yeo Kheng Meng <yeokm1@gmail.com>
2014-12-17 18:19:04 +0800

    basic help stuff

--- app/src/main/java/com/yeokm1/nussocprintandroid/fragments/HelpFragment.java
+++ app/src/main/java/com/yeokm1/nussocprintandroid/fragments/HelpFragment.java
@@ -1,28 +1,70 @@
 package com.yeokm1.nussocprintandroid.fragments;
 
 
-
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.yeokm1.nussocprintandroid.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HelpFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+import java.text.SimpleDateFormat;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class HelpFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        String helpText = generateHelpText();
+        TextView helpTextView = (TextView) view.findViewById(R.id.help_textview);
+        helpTextView.setText(helpText);
+        return view;
+    }
+
+
+    String generateHelpText(){
+        String textFormat = getString(R.string.help_text);
+        String compileTime = getCompileDateTime();
+        String packageDetails = getPackageVersion();
+        String helpText = String.format(textFormat, packageDetails, compileTime);
+        return helpText;
+    }
+
+    String getCompileDateTime(){
+        try{
+            ApplicationInfo ai = getActivity().getPackageManager().getApplicationInfo(getActivity().getPackageName(), 0);
+            ZipFile zf = new ZipFile(ai.sourceDir);
+            ZipEntry ze = zf.getEntry("classes.dex");
+            long time = ze.getTime();
+            String s = new SimpleDateFormat("dd MMM yyyy").format(new java.util.Date(time));
+            zf.close();
+            return s;
+        }catch(Exception e){
+        }
+
+        return "";
+    }
+
+
+    String getPackageVersion(){
+        try {
+            String packageName = getActivity().getApplicationContext().getPackageName();
+            PackageInfo pi = getActivity().getPackageManager().getPackageInfo(packageName, PackageManager.GET_META_DATA);
+            int versionCode = pi.versionCode;
+            String versionName = pi.versionName;
+            return  versionName + "," + versionCode;
+        } catch (Exception e) {
+            return "";
+        }
     }
 
 
